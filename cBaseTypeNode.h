a74cb189fd3db14479fcb6ed1aa5d4c567f8566f
mark.shanklin <mark.shanklin@oit.edu>
2016-02-21 19:36:55 -0800

    Lab5 passes through test 2b

---
+++ cBaseTypeNode.h
@@ -0,0 +1,33 @@
+#pragma once
+/*******************************************
+ * Author: Mark Shanklin
+ * mark.shanklin@oit.edu
+ *
+ * Date: Feb. 20, 2016
+ ******************************************/
+#include <string>
+#include "cDeclNode.h"
+class cBaseTypeNode : public cDeclNode
+{
+    public:
+        cBaseTypeNode(string name, int size, bool is_float)
+        {
+            m_name = name;
+            m_size = size;
+            m_is_float = is_float;
+        }
+        virtual bool isVar(){ return false; }
+        virtual bool isFunc(){ return false; }
+        virtual bool isFuncCall(){ return false; }
+        virtual bool isStruct(){ return false; }
+        virtual bool isFloat(){ return m_is_float; }
+        virtual int GetSize(){ return m_size; }
+        virtual string GetName(){ return m_name; }
+        virtual cDeclNode* GetType(){ return this; }    
+        virtual string NodeType() { return string("type"); }
+        virtual void Visit(cVisitor *visitor) { visitor->Visit(this); }
+    protected:
+        string m_name;
+        int m_size;
+        bool m_is_float;
+};
